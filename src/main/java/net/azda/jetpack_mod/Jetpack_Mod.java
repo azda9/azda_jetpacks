92c0f75f681f931f42c7e6a4093052a3c242edf6
azda <azda7050@gmail.com>
2024-02-10 04:03:20 -0500

    jetpack movement added

--- src/main/java/net/azda/jetpack_mod/Jetpack_Mod.java
+++ src/main/java/net/azda/jetpack_mod/Jetpack_Mod.java
@@ -2,9 +2,11 @@ package net.azda.jetpack_mod;
 
 import com.mojang.logging.LogUtils;
 import net.azda.jetpack_mod.item.ModItems;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
@@ -16,9 +18,11 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -33,7 +37,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.joml.Vector3d;
 import org.slf4j.Logger;
+
+import javax.swing.text.JTextComponent;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Jetpack_Mod.MOD_ID)
@@ -80,21 +87,31 @@ public class Jetpack_Mod
     {
 
     }
-
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
+    @Mod.EventBusSubscriber(modid = MOD_ID)
+    public static class ForgeEvents
     {
-
-    }
-
-    @SubscribeEvent
-    public static void onLivingHurt(LivingHurtEvent event)
-    {
-        if(event.getEntity() instanceof Sheep) {
-            if(event.getSource().getEntity() instanceof Player player) {
-                player.lerpMotion(0,1,0);
-                player.sendSystemMessage(Component.literal(player.getName().getString()+"you hurted that hseep"));
+        @SubscribeEvent
+        public static void onPlayerTick(TickEvent.PlayerTickEvent event)
+        {
+            var player = event.player;
+            var playerLook = player.getLookAngle();
+            if (Minecraft.getInstance().player != null) {
+                if (Minecraft.getInstance().player.input.jumping) {
+                    player.addDeltaMovement(new Vec3(0, 0.1, 0));
+                }
+                if (Minecraft.getInstance().player.input.up) {
+                    player.addDeltaMovement(new Vec3(0.05 * playerLook.x, 0, 0.05 * playerLook.z));
+                }
+                if (Minecraft.getInstance().player.input.down) {
+                    player.addDeltaMovement(new Vec3(-0.05 * playerLook.x, 0, -0.05 * playerLook.z));
+                }
+                if (Minecraft.getInstance().player.input.right) {
+                    player.addDeltaMovement(new Vec3(-0.05 * playerLook.z, 0, 0.05 * playerLook.x));
+                }
+                if (Minecraft.getInstance().player.input.left) {
+                    player.addDeltaMovement(new Vec3(0.05 * playerLook.z, 0, -0.05 * playerLook.x));
+                }
             }
         }
     }
